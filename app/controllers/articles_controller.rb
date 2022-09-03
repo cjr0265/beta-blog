ba605b239e6bccc932c33a94ba1eef3306e32d87
cjr0265 <cjr0265@unt.edu>
2022-09-02 18:11:23 -0600

    Add new, create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,14 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+  end
+
   def index
     @articles = Article.all
+  end
+
+  def create
   end
 
 end
